e9a1cdd44ae8741c52c90fbe9bc4eed19de4091c
HoangMinhDuc <hoangminhduc4125@gmail.com>
2024-09-02 20:44:42 +0700

    fix

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json())
-app.use(cors)
+app.use(cors())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',   
  'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
